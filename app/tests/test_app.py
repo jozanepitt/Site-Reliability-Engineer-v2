9c8e51fc624aa59febb7b990d1c4aff83e7448c4
jozanepitt <69273769+jozanepitt@users.noreply.github.com>
2024-10-22 18:33:06 +0200

    Update test_app.py

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,15 +1,14 @@
 import unittest
-from app import app
+from mycompany_api.main import app
 
-class AppTestCase(unittest.TestCase):
+class MyCompanyAPITestCase(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
 
-    def test_index(self):
-        response = self.client.get('/')
+    def test_hello_world(self):
+        response = self.client.get('/api/v1/hello')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Hello, World!', response.data)
+        self.assertIn(b'Welcome to MyCompany API!', response.data)
 
 if __name__ == '__main__':
     unittest.main()
-
