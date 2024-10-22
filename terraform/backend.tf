6528f94dd0b6c3bdc6fe86fc6bee8097af4abe09
jozanepitt <69273769+jozanepitt@users.noreply.github.com>
2024-10-22 18:40:46 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "mycompany-terraform-state"
+    key            = "production/mycompany-api-infrastructure.tfstate"
+    region         = var.aws_region
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
 
