e522ddc9b694475b1b360f1e0993dc024271c7ec
jozanepitt <69273769+jozanepitt@users.noreply.github.com>
2024-10-22 18:38:43 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,13 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "rds_endpoint" {
+  value     = module.rds.endpoint
+  sensitive = true
+}
 
